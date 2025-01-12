6494632c1fa227f5437ac7b68178070df9ee816a
Bruno Gomes <obrunogomes@obrunogomes.local>
2025-01-12 18:35:05 -0300

    config project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 
-import './App.css'
 
-function App() {
+export function App() {
 
   return (
     <>
-      <main>
-        <h1>Seja bem vindo! :)</h1>
-        <p>Desafio de Galeria de Imagens - Processo Seletivo Frontend Fusion</p>
-      </main>
+      <h1>ola mundo</h1>
     </>
   );
 }
 
-export default App
+
